5494f5bf4c471966a1cfc23c16c734d984677bed
cauacozz11 <cauacozz@gmail.com>
2025-08-11 10:53:33 -0300

    uau

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 # Importação
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# Modelagem
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, )
 
 # Definir uma rota raiz (página inicial) e a fnção que será executada ao requisitar
-@app.route('/')
-def hello_word():
+@app.route('/teste')
+def hello_world():
     return 'Hello World'
 
 if __name__ == "__main__":
